0ba149eec8d5801091aa09e84232bc0941064270
kyle java <42562960+kylejava@users.noreply.github.com>
2022-10-16 00:24:13 -0700

    Added New Page

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ import os
 import json
 import string
 import random
+import sys
 from pprint import pprint
 from flask import Flask, render_template, flash, request, redirect
 import webbrowser
+import base64
 app = Flask(__name__)
 
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 REDIRECT_URI = os.environ.get("REDIRECT_URI")
 SCOPE = os.environ.get("SCOPE")
+ENCODEDSTRING = os.environ.get("ENCODEDSTRING")
 
-@app.route('/', methods = ['GET', 'POST'])
+
+@app.route('/', methods = ["GET", "POST"])
 def home():
     print(CLIENT_ID)
     return render_template('index.html')
@@ -42,13 +46,14 @@ def callback():
     auth_url="https://accounts.spotify.com/api/token"
     requested_data = requests.post(auth_url, data=params, headers = HEADERS)
     final_data = requested_data.json()
+    print(final_data)
     if("error" in final_data):
         return redirect('/')
     return display_data(final_data['access_token'])
 
 @app.route('/display',methods = ['GET', 'POST'])
 def display(my_token):
-    user_ = requests.get("https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=20&offset=1", headers=HEADERS)
+    user_ = requests.get("https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=50&offset=1", headers=HEADERS)
     user_ = user_.json()
     tracks = []
     for item in user_['items']:
@@ -68,15 +73,9 @@ def display(my_token):
 
 
 
-    while(len(tracks) != 8):
-        album = {
-            "name": "none",
-            "album": "none",
-            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1982px-Spotify_icon.svg.png"
-        }
-        tracks.append(album)
 
-    return "Test"
+
+    return render_template("index.html")
 
 
 
